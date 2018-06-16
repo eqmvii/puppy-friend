993e6ca8daedad610b36dd7fd1c9f8575c171692
Eric (Martin) Mancini <coelomate@gmail.com>
2018-06-16 10:27:19 -0400

    roughed in random doggo

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 // const Search = () => (
 //   <div>
@@ -10,6 +11,7 @@ import React, { Component } from "react";
 class Search extends Component {
   state = {
     term: "",
+    image: ""
   };
 
   // Highly generic input change handling function
@@ -21,13 +23,31 @@ class Search extends Component {
     });
   };
 
+  searchPuppies = query => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+      .then((response) => {
+        console.log(response);
+        console.log(response.data.message);
+        this.setState({ image: response.data.message });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    // API.search(query)
+    //   .then(res => this.setState({ results: res.data.data }))
+    //   .catch(err => console.log(err));
+  };
+
   handleSearchSubmit = event => {
     event.preventDefault();
     alert(`Searching for: ${this.state.term}`);
+    this.searchPuppies(this.state.term);
     this.setState({ term: "" });
+
   };
 
   render() {
+    var image = this.state.image ? (<div><h1>Random Doggo</h1><div className="img-container"><img alt="puppy" src={this.state.image} /></div></div>): null
     return (<div>
       <h1>Search for a doggo</h1>
       <input
@@ -38,7 +58,7 @@ class Search extends Component {
         onChange={this.handleInputChange}
       />
       <button onClick={this.handleSearchSubmit}>Search</button>
-
+      {image}
     </div>)
   }
 }
